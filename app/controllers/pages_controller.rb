a68bfc1c637f280e68c6732b15fd4002ab254204
Damon Roberts <damonspencerroberts@gmail.com>
2021-03-03 23:46:39 +0000

    pagenate search and color code code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
 
   def search
     @query = params[:query]
-    @use_cases = UseCase.search_by_title_and_description_and_category(@query)
+    @use_cases = UseCase.search_by_title_and_description_and_category(@query).paginate(page: params[:page], per_page: 5)
+    @use_cases_total = UseCase.search_by_title_and_description_and_category(@query).size
   end
 end
